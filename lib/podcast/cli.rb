f4a7845069394d7e72563e68bc56df40ffbad259
Nate Dietz <nsdietz@gmail.com>
2018-06-29 09:09:59 -0400

    cli methods established

--- lib/podcast/cli.rb
+++ lib/podcast/cli.rb
@@ -1,14 +1,37 @@
 class Podcast::CLI
 
   def call
-    puts "Here is a list of NPR podcasts"
     list_podcasts
+    menu
+    goodbye
   end
 
   def list_podcasts
-    puts "here is list of podcasts. which one would you like more info?"
+    puts "welcome to the podcast gem! here is list of podcasts:"
+    puts "1. Car Talk - Games & Hobbies"
+    puts "2. Fresh Air - Arts"
   end
 
+  def menu
+    input = nil
 
+    while input != "exit"
+      puts "enter the number for the podcast that you would you like more information, type list to see all the podcasts or type exit:"
+      input = gets.strip.downcase
+      if input == "1"
+        puts "more info on Car Talk"
+      elsif input == "2"
+        puts "more info on Fresh Air"
+      elsif input == "list"
+        list_podcasts
+      else
+        puts "invalid input. type list or exit"
+      end
+    end
+  end
+
+  def goodbye
+    puts "see you later"
+  end
 
 end
