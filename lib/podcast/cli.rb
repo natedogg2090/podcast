36b6b386666de23ebb136ffed4cff828ac6ae189
Nate Dietz <nsdietz@gmail.com>
2018-06-30 17:17:49 -0400

    refactoring of CLI class

--- lib/podcast/cli.rb
+++ lib/podcast/cli.rb
@@ -3,11 +3,12 @@ class Podcast::CLI
   def call
     list_podcasts
     menu
-    goodbye
   end
 
   def list_podcasts
-    puts "welcome to the podcast gem! here is list of podcasts:"
+    puts "Welcome to my podcast gem!"
+    puts ""
+    puts "Here is list of NPR podcasts:"
     @podcast = Podcast::PodcastScraper.scrape_npr_directory
     @podcast.each.with_index(1) do |podcast, i|
       puts "#{i}. #{podcast.name} - #{podcast.category}"
@@ -18,14 +19,17 @@ class Podcast::CLI
     input = nil
 
     while input != "exit"
-      puts "enter the number for the podcast that you would you like more information, type list to see all the podcasts or type exit:"
+      puts ""
+      puts "Enter the number for the podcast that you would you like more information, type 'list' to see all the podcasts or type 'exit':"
       input = gets.strip.downcase
       if input.to_i > 0
         podcast_details(input)
       elsif input == "list"
         list_podcasts
+      elsif input == "exit"
+        goodbye
       else
-        puts "invalid input. type list or exit"
+        puts "Invalid input. Please enter a valid number, type 'list' for the list of podcasts or type 'exit'"
       end
     end
   end
@@ -45,7 +49,9 @@ class Podcast::CLI
   end
 
   def goodbye
-    puts "see you later"
+    puts ""
+    puts "See you next time."
+    puts ""
   end
 
 end
